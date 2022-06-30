efa738fc7d83ed068e92a4620ca9ed9cb937c451
jeffersoneva <105385318+jeffersoneva@users.noreply.github.com>
2022-06-30 18:29:21 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,170 @@
+<!doctype html>
+<html lang="pt-br">
+
+	<head>
+
+    <meta charset="UTF-8" />
+        
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no" />
+        <meta name="description" content="Formatos de texto em HTML, desenvolvido por Jefferson Eva" />
+        <meta name="author" content="Jefferson Eva" />
+        <meta name="keywords" content="textos, web, desenvolvimento, html, escrita">
+
+		<meta name="theme-color" content="#2F4858">
+		<meta name="apple-mobile-web-app-status-bar-style" content="#2F4858">
+		<meta name="msapplication-navbutton-color" content="#2F4858">
+
+        <link rel="preload" href="css/estilo.css" as="style">
+		<link rel="stylesheet" href="css/estilo.css">
+
+        <link rel="icon" href="../../img/favicons/favicon-16.png" sizes="16x16">
+		<link rel="icon" href="../../img/favicons/favicon-24.png" sizes="24x24">
+		<link rel="icon" href="../../img/favicons/favicon-32.png" sizes="32x32">
+		<link rel="icon" href="../../img/favicons/favicon-48.png" sizes="48x48">
+		<link rel="icon" href="../../img/favicons/favicon-64.png" sizes="64x64">
+		<link rel="icon" href="../../img/favicons/favicon-72.png" sizes="72x72">
+		<link rel="icon" href="../../img/favicons/favicon-96.png" sizes="96x96">
+		<link rel="icon" href="../../img/favicons/favicon-114.png" sizes="114x114">
+		<link rel="icon" href="../../img/favicons/favicon-128.png" sizes="128x128">
+		<link rel="icon" href="../../img/favicons/favicon-144.png" sizes="144x144">
+
+        <title>Formatação de Texto em HTML</title>
+        
+    </head>
+
+    <body>
+
+        <table>
+
+            <thead>
+                <tr><th colspan="2">Tags de Formatação de Texto</th></tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>&lt;b&gt;</td>
+                    <td><b>texto em negrito</b></td>
+                </tr>
+                <tr>
+                    <td>&lt;big&gt;</td>
+                    <td><big>texto grande</big></td>
+                </tr>
+                <tr>
+                    <td>&lt;em&gt;</td>
+                    <td><em>texto enfatizado</em></td>
+                </tr>
+                <tr>
+                    <td>&lt;i&gt;</td>
+                    <td><i>texto em itálico</i></td>
+                </tr>
+                <tr>
+                    <td>&lt;small&gt;</td>
+                    <td><small>texto pequeno</small></td>
+                </tr>
+                <tr>
+                    <td>&lt;strong&gt;</td>
+                    <td><strong>texto forte</strong></td>
+                </tr>
+                <tr>
+                    <td>&lt;sub&gt;</td>
+                    <td><sub>texto subescrito</sub></td>
+                </tr>
+                <tr>
+                    <td>&lt;sup&gt;</td>
+                    <td><sup>texto superescrito</sup></td>
+                </tr>
+                <tr>
+                    <td>&lt;ins&gt;</td>
+                    <td><ins>texto inserido</ins></td>
+                </tr>
+                <tr>
+                    <td>&lt;del&gt;</td>
+                    <td><del>texto cancelado</del></td>
+                </tr>
+            </tbody>
+
+            <thead>
+                <tr><th colspan="2">Tags de "Saída de Computador"</th></tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>&lt;code&gt;</td>
+                    <td><code>texto de código de computador</code></td>
+                </tr>
+                <tr>
+                    <td>&lt;kbd&gt;</td>
+                    <td><kbd>texto de teclado</kbd></td>
+                </tr>
+                <tr>
+                    <td>&lt;samp&gt;</td>
+                    <td><samp>amostra de código de computador</samp></td>
+                </tr>
+                <tr>
+                    <td>&lt;tt&gt;</td>
+                    <td><tt>texto de teletipo</tt></td>
+                </tr>
+                <tr>
+                    <td>&lt;var&gt;</td>
+                    <td><var>texto de variável</var></td>
+                </tr>
+                <tr>
+                    <td>&lt;pre&gt;</td>
+                    <td><pre>texto pré-formatado</pre></td>
+                </tr>
+            </tbody>
+
+            <thead>
+                <tr><th colspan="2">Tags de Citações</th></tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>&lt;abbr&gt;</td>
+                    <td><abbr>uma abreviatura</abbr></td>
+                </tr>
+                <tr>
+                    <td>&lt;kbd&gt;</td>
+                    <td><kbd>uma abreviatura</kbd></td>
+                </tr>
+                <tr>
+                    <td>&lt;acronym&gt;</td>
+                    <td><acronym>um acrônimo</acronym></td>
+                </tr>
+                <tr>
+                    <td>&lt;address&gt;</td>
+                    <td><address>um elemento de endereço</address></td>
+                </tr>
+                <tr>
+                    <td>&lt;bdo&gt;</td>
+                    <td><bdo>a direção do texto</bdo></td>
+                </tr>
+                <tr>
+                    <td>&lt;blockquote&gt;</td>
+                    <td><blockquote>uma citação longa</blockquote></td>
+                </tr>
+                <tr>
+                    <td>&lt;q&gt;</td>
+                    <td><q>uma citação curta</q></td>
+                </tr>
+                <tr>
+                    <td>&lt;cite&gt;</td>
+                    <td><cite>uma citação</cite></td>
+                </tr>
+                <tr>
+                    <td>&lt;dfn&gt;</td>
+                    <td><dfn>um termo de definição</dfn></td>
+                </tr>
+            </tbody>
+
+        </table>
+
+
+        <div class="footer">
+            Copyright&copy; - <a href="https://www.jeffersoneva.com">www.jeffersoneva.com</a> <?php echo date("Y"); ?>
+        </div>
+
+        
+    </body>
+
+</html>
